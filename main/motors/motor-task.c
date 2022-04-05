a66b5d8174d55709b3593c37b6f66d6bca8326de
Tomas Vitek <tomas.vitek.tvt@gmail.com>
2022-04-05 19:38:40 +0200

    Added second motor

--- main/motors/motor-task.c
+++ main/motors/motor-task.c
@@ -10,10 +10,10 @@
 
 void motor_task(void *args) {
     motor_config_t m1Cfg = {
-        .stepPin = MOTOR_STEP_PIN,
-        .dirPin = MOTOR_DIR_PIN,
-        .cfg1Pin = MOTOR_CFG1_PIN,
-        .cfg2Pin = MOTOR_CFG2_PIN,
+        .stepPin = MOTOR_DEC_STEP_PIN,
+        .dirPin = MOTOR_DEC_DIR_PIN,
+        .cfg1Pin = MOTOR_DEC_CFG1_PIN,
+        .cfg2Pin = MOTOR_DEC_CFG2_PIN,
         .maxA = MOTOR_MAX_A,
         .maxV = MOTOR_MAX_V,
         .brakeA = MOTOR_BRAKE_A,
@@ -21,30 +21,48 @@ void motor_task(void *args) {
         .gotoMinV = MOTOR_GOTO_MIN_V,
         .minStepI = MOTOR_MIN_STEP_I_MICROS
     };
-    motor_t motor1 = motor_create(m1Cfg);
+    motor_t mDec = motor_create(m1Cfg);
+
+    motor_config_t m2Cfg = {
+        .stepPin = MOTOR_RA_STEP_PIN,
+        .dirPin = MOTOR_RA_DIR_PIN,
+        .cfg1Pin = MOTOR_RA_CFG1_PIN,
+        .cfg2Pin = MOTOR_RA_CFG2_PIN,
+        .maxA = MOTOR_MAX_A,
+        .maxV = MOTOR_MAX_V,
+        .brakeA = MOTOR_BRAKE_A,
+        .aPosK = MOTOR_A_POS_K,
+        .gotoMinV = MOTOR_GOTO_MIN_V,
+        .minStepI = MOTOR_MIN_STEP_I_MICROS
+    };
+    motor_t mRa = motor_create(m2Cfg);
+
     ESP_LOGI(TAG, "Motor task started");
     int64_t timeGoal = esp_timer_get_time() + 15000000;
-    motor_track(motor1, 500, 10000, esp_timer_get_time(), timeGoal);
-    //motor_goto(motor1, -200000);
+    //motor_track(mDec, 500, 10000, esp_timer_get_time(), timeGoal);
+    motor_track(mRa, 500, 10000, esp_timer_get_time(), timeGoal);
+    motor_goto(mDec, -300000);
     int64_t tLastUpdate = esp_timer_get_time();
     int64_t maxExecT = 0;
     for(;;) {
         int64_t t1 = esp_timer_get_time();
-        motor_run(motor1);
+        motor_run(mDec);
+        motor_run(mRa);
         int64_t t2 = esp_timer_get_time();
         if (maxExecT < t2 - t1)
             maxExecT = t2 - t1;
         if (t2 > tLastUpdate + MOTOR_TSK_UPADTE_P) {
             tLastUpdate = t2;
-            uint64_t posOffset = motor_getPosOffset(motor1, t1);
+            uint64_t posOffset = motor_getPosOffset(mDec, t1);
             time_t time;
             int64_t motorT = esp_timer_get_time();
-            //ESP_LOGD(TAG, "M max exec t: %lli micros, posOffset: %lli, mode: %i, v: %f, p: %lli, tpos: %lli", maxExecT, posOffset, motor1->mode, motor1->v, motor1->pos, motor1->tPos);
+            //ESP_LOGD(TAG, "M max exec t: %lli micros, posOffset: %lli, mode: %i, v: %f, p: %lli, tpos: %lli", maxExecT, posOffset, mDec->mode, mDec->v, mDec->pos, mDec->tPos);
             maxExecT = 0;
             mount_getTime(&time);
-            motor_track(motor1, time * 5 - MOTOR_TSK_UPADTE_P / 1000 * 5, time * 5, motorT, motorT + MOTOR_TSK_UPADTE_P);
+            //motor_track(mDec, time * 5 - MOTOR_TSK_UPADTE_P / 1000 * 5, time * 5, motorT, motorT + MOTOR_TSK_UPADTE_P);
+            motor_track(mRa, time * 5 - MOTOR_TSK_UPADTE_P / 1000 * 5, time * 5, motorT, motorT + MOTOR_TSK_UPADTE_P);
         }
     }
 
-    motor_destroy(motor1);
+    motor_destroy(mDec);
 }
